eca291ab371a68dbbf8e7135e83bd325a9dc1544
Suhani S Rao <suharao21@gmail.com>
2025-02-26 13:23:21 +0530

    LinkedHashMaps.java

--- maps.java
+++ maps.java
@@ -1,14 +1,14 @@
-import java.util.TreeMap;
+import java.util.*;
 import java.util.Map;
 public class maps
 {
     public static void main(String arg[])
     {
-        Map<Integer,String> ft=new HashMap<>();
+        LinkedHashMap<Integer,String> ft=new LinkedHashMap<>();
         ft.put(1,"Apple");
         ft.put(2,"Orange");
-        ft.put(3,"Bannana");
-        ft.put(4,"Watermellon");
+        ft.put(4,"Bannana");
+        ft.put(3,"Watermellon");
         System.out.println("Fruits : "+ft);
         System.out.println("3rd value of fruits Array : "+ft.get(3));
         ft.remove(3);
